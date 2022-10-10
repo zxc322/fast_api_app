40b3a6bbcc27d7e56a9152c12747cdd9e29645cc
zxc_322 <kvasoliaeww@gmail.com>
2022-10-10 16:44:40 +0300

    create poetry env/add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'status': 'Working'}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)

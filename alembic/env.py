d1df281efce486363c36fdce653230692df69b2c
zxc_322 <kvasoliaeww@gmail.com>
2022-10-13 23:28:20 +0300

    replace hardcoded url with url from env in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,12 @@
 from logging.config import fileConfig
-
+from os import getenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
 from db.models import Base
+from db.base import DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,8 +27,8 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-
+ 
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -40,6 +41,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
+       
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,

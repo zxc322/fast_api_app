f65ea73d85ac9a70ce4843bada839aa19afc0ebf
zxc_322 <kvasoliaeww@gmail.com>
2022-10-13 22:02:28 +0300

    change project tree/add User model, schemas/add alembic

---
+++ db/models.py
@@ -0,0 +1,23 @@
+from email.policy import default
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow) 
+    #updated_by = relationship("User", back_populates="users", default=1)
+
+
+
--- src/db/base.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-
-from db.connection import DATABASE_URL
-
-
-metadata = sqlalchemy.MetaData()
-engine = sqlalchemy.create_engine(DATABASE_URL)
-metadata.create_all(engine)
